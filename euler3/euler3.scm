2c095db4d1d84810382a43b072c0d1a9a1da736e
Kellen Barber <kellenbarber@email.com>
2018-05-17 16:15:07 -0700

    divides

--- euler3/euler3.scm
+++ euler3/euler3.scm
@@ -1,3 +1,11 @@
 ;; euler3.scm
 ;;The prime factors of 13195 are 5, 7, 13 and 29.
 ;; What is the largest prime factor of the number 600851475143 ?
+
+(define (divides num divisor)
+  (= (modulo num divisor) 0)
+)
+
+(define (is-prime num)
+ 
+)
